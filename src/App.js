6786bf0d5022fb5590bad3daf7cd9597c934f57c
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-11-10 12:00:14 +0300

    project folder and disagne

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ReactDOM } from 'react-dom/client';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 import './App.css';
-
+import 'tachyons';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navigation/>
+     <Logo/>
+   
+     {/* 
+      
+      <ImageLinkFrom/>
+      <FaceRecognition/>
+      */}
     </div>
   );
 }
