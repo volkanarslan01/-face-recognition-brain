682072454debd5478675a141d008a80c32ea37df
volkanarslan01 <101024530+volkanarslan01@users.noreply.github.com>
2022-12-19 08:19:13 +0300

    Clarifa Api Connect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from "react";
 import Navigation from "./components/Navigation/Navigation";
+import Clarifai from "clarifai";
 import ImageLinkFrom from "./components/ImageLinkFrom/ImageLinkFrom";
 import Rank from "./components/Rank/Rank";
 import Logo from "./components/Logo/Logo";
 import "./App.css";
 import "tachyons";
+import FaceRecognition from "./components/FaceRecognition/FaceRecognition.js";
+import Particle from "./components/Particles";
+
+const app = new Clarifai.App({
+  apiKey: "e908dde636da467285c367ce62b6bcf8",
+});
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      input: "",
+      input: ``,
+      imageUrl: ``,
+      box: {},
     };
   }
-  onButtonSubmit = () => {
-    console.log("Click");
+  calculateFaceLocation = (data) => {
+    const clarifaiFace =
+      data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById("inputImage");
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
   };
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.GENERAL_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      )
+      .catch((error) => console.log(error));
+  };
+
   render() {
     return (
       <div className="App">
+        <Particle />
         <Navigation />
         <Logo />
         <Rank />
@@ -28,11 +63,8 @@ class App extends React.Component {
           onInputChange={this.onInputChange}
           onButtonSubmit={this.onButtonSubmit}
         />
-        {/* 
-      
-     
-      <FaceRecognition/>
-      */}
+
+        <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -2,7 +2,7 @@ import React from "react";
 const Rank = () => {
   return (
     <div>
-      <div className="white f3">{"Volkan, your current rank is ..."}</div>
+      <div className="white f3">{"Volkan, your current rank is..."}</div>
       <div className="white f3">{"#5"}</div>
     </div>
   );
